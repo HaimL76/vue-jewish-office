cb72f064db9b5d1e7a01d5cd6fa1af1189f224d5
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2021-01-05 23:10:04 +0200

    3

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 import HelloWorld from "./components/HelloWorld.vue";
 import UserInfo from "./components/UserInfo.vue";
 import UserPersonal from "./components/UserPersonal.vue";
 //import Home from "./components/Home.vue";
 //import Ingredients from "./components/Ingredients.vue";
 //import Recipes from "./components/Recipes.vue";
-const history = createWebHistory();
+const history = createWebHashHistory();
 const routes = [
     { path: "/", component: HelloWorld },
     { path: "/info", component: UserInfo },
