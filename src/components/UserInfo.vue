8213e60ffa8d5d848481274303d8473659a29b07
HaimL76 <40207748+HaimL76@users.noreply.github.com>
2021-01-08 08:32:54 +0200

    4

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,24 +1,82 @@
 <template>
   <h1>מידע אישי</h1>
   <div>
-שם פרטי {{person.first}}
-שם משפחה {{person.last}}
+    שם פרטי {{person.first}}
+    שם משפחה {{person.last}}
   </div>
 </template>
 
+
 <script>
-  import { ref, reactive } from 'vue'
+import { ref, reactive, onMounted } from "vue";
+export default {
+  name: 'UserInfo',
+  props: {
+  },
+  setup() {
+    const data = ref(null);
+    const loading = ref(true);
+    const error = ref(null);
+    const person = reactive({
+      first: ref(null), 
+      last: ref(null)
+    });
 
-  export default {
-    setup() {
-      const person = reactive({ first: 'זלמן', last: 'פוזננסקי' });
-      const dummy = ref(0);
+function fetchData() {
+    loading.value = true;
+    // I prefer to use fetch
+    // you can use use axios as an alternative
+    return fetch('http://jsonplaceholder.typicode.com/posts', {
+      method: 'get',
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+    .then(res => {
+      // a non-200 response code
+      if (!res.ok) {
+        // create error instance with HTTP status text
+        const error = new Error(res.statusText);
+        error.json = res.json();
+        throw error;
+      }
 
-      // expose to template
-      return {
-        person,
-        dummy
-      };
-    }
+      return res.json();
+    })
+    .then(json => {
+      // set the response data
+      data.value = json.data;
+
+      //alert(JSON.stringify(data.value));
+    })
+    .catch(err => {
+      error.value = err;
+
+      //alert(JSON.stringify(error.value));
+
+      // In case a custom JSON error response was provided
+      if (err.json) {
+        return err.json.then(json => {
+          // set the JSON response message
+          error.value.message = json.message;
+        });
+      }
+    })
+    .then(() => {
+      loading.value = false;
+    });
+}
+    onMounted(() => {
+      fetchData();
+    });
+
+    return {
+      data,
+      loading,
+      error,
+      person
+    };
   }
+}
 </script>
+
